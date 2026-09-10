5f49fc093e9af1552b16987fd050d5f92a370b73
Ruben Marin <rubenmarin00@gmail.com>
2026-09-10 18:54:24 +0200

    docs: explain Spring Boot context test

--- src/test/java/com/rubenmarin/climbingmanagementsb/ClimbingManagementSbApplicationTests.java
+++ src/test/java/com/rubenmarin/climbingmanagementsb/ClimbingManagementSbApplicationTests.java
@@ -4,6 +4,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
+// "Can Spring Boot start my application successfully?"
+// When you create a Spring Boot project using the standard project generator, it commonly creates this test automatically.
+// It's essentially a smoke test.
+
 @SpringBootTest
 @ActiveProfiles("test")
 class ClimbingManagementSbApplicationTests {
